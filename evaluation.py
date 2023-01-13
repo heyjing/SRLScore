b40d3be9236defcd9df05f07b0a4f583f2da8bd7
Jing Fan <jfan@eirene.ifi.uni-heidelberg.de>
2023-01-13 09:00:08 +0100

    integrated pipeline

--- evaluation.py
+++ evaluation.py
@@ -1,108 +1,95 @@
 """
-This script evaluates the test set of dataset. 
+This script 
+1. evaluates if a summary is factual consistent. It receives the summary text and the source text 
+and generates a factual consistency score of the summary.
 """
 
 from typing import List
+from numbers import Number
 from statistics import mean
 import random
-import sys
-import os
+import warnings
 
+from tqdm import tqdm
 from datasets import load_dataset
-
 import extract_tuples as et
+import tuple_comparison as tc
 
 
-def get_dailymail_samples(
-    do_filtering: bool, sample_num: int = 15, split: str = "train",
-) -> List[dict]:
-    """
-    This script randomly select articles from the cnn_dailymail dataset.
-    """
-    data = load_dataset("cnn_dailymail", "3.0.0")[split]
+class ModelConfigurator:
+    def __init__(self, coref_solver, func_words_remover, string_comparison_method):
+        self.coref_solver = coref_solver
+        self.func_words_remover = func_words_remover
+        self.string_comparison_method = string_comparison_method
 
-    if do_filtering == True:
+    def calculate_summary_score(
+        self, source_text: str, generated_summary: str
+    ) -> float:
+        """
+        This function builds a SRL tuple database for source text and generated summary, and
+        calculates the consistency score of the generated summary.
+        """
 
-        my_file = open(
-            os.path.join(sys.path[0], "dailymail_faithful_examples.txt"), "r"
+        if len(source_text.split(" ")) >= 450:
+            warnings.warn(
+                "Source text might exceed input length limit for SRL extraction!"
+            )
+        if len(generated_summary.split(" ")) >= 450:
+            warnings.warn(
+                "Generated summary might exceed input length limit for SRL extraction!"
+            )
+
+        print("-----extract tuples from summary text-----")
+        generated_tuples: List[tuple] = et.extract_tuples(
+            generated_summary, self.coref_solver, self.func_words_remover
         )
+        print("---generated tuples are---:", generated_tuples)
 
-        # reading the file
-        # replacing end splitting the text
-        # when newline ('\n') is seen.
-        data_into_list = my_file.read().split("\n")
-        my_file.close()
+        print("-----extract tuples from source text-----")
+        source_tuples: List[tuple] = et.extract_tuples(
+            source_text, self.coref_solver, self.func_words_remover
+        )
+        print("---source tuples are---：", source_tuples)
 
-        data = data.filter(lambda example: example["id"] in data_into_list)
+        tuple_final_scores = []
+        for generated_tup in tqdm(generated_tuples, desc="calculate_summary_score"):
+            tuple_final_score = tc.compare_tuple_with_relevant_tuples(
+                source_tuples, generated_tup, self.string_comparison_method,
+            )
+            tuple_final_scores.append(tuple_final_score)
+        print("---Score of each tuple in generated text：", tuple_final_scores)
 
-    print(len(data))
-    random.seed(42)
-    samples: List[dict] = random.sample(list(data), sample_num)
-    # short_samples = [
-    #     sample for sample in samples if len(sample["article"].split(" ")) < 350
-    # ]
+        summary_score = round(mean(tuple_final_scores), 2)
+        return summary_score
 
-    print(samples)
+    def evaluate_dailymail(
+        self, sample_num: Number = 15, split: str = "train"
+    ) -> List[float]:
+        """
+        This script randomly select sample articles from the cnn_dailymail dataset.
+        Then it applys the calculate_summary_score function many times for each random sample article.
+        """
+        data = load_dataset("cnn_dailymail", "3.0.0")[split]
+        random.seed(42)
+        samples: List[dict] = random.sample(list(data), sample_num)
+        short_samples = [
+            sample for sample in samples if len(sample["article"].split(" ")) < 400
+        ]
+        print(short_samples)
 
-    return samples
-
-
-def get_xsum_samples(
-    do_filtering: bool, sample_num: int = 15, split: str = "train"
-) -> List[dict]:
-    """
-    This script randomly select sample articles from the cnn_dailymail dataset.
-    """
-    data = load_dataset("xsum")[split]
-    if do_filtering == True:
-        my_file = open(os.path.join(sys.path[0], "xsum_faithful_examples.txt"), "r")
-
-        # reading the file
-        # replacing end splitting the text
-        # when newline ('\n') is seen.
-        data_into_list = my_file.read().split("\n")
-        my_file.close()
-
-        data = data.filter(lambda example: example["id"] in data_into_list)
-
-    print(len(data))
-    random.seed(42)
-    samples: List[dict] = random.sample(list(data), sample_num)
-    # short_samples = [
-    #     sample for sample in samples if len(sample["document"].split(" ")) < 350
-    # ]
-    print(samples)
-
-    return samples
+        scores = [
+            self.calculate_summary_score(sample["article"], sample["highlights"])
+            for sample in short_samples
+        ]
+        print(scores)
 
 
 if __name__ == "__main__":
-    model = et.ModelConfigurator(
-        use_coref_solver=False,
-        use_func_words_remover=True,
-        string_comparison_method="spacy",
+    model = ModelConfigurator(
+        coref_solver=False, func_words_remover=False, string_comparison_method="exact",
     )
-    samples = get_dailymail_samples(do_filtering=False)
+    model.evaluate_dailymail()
 
-    scores = [
-        model.calculate_summary_score(sample["article"], sample["highlights"])
-        for sample in samples
-    ]
-    print("number of samples: ", len(samples))
-    print("scores: ", scores)
-    print("mean score: ", mean(scores))
-
-    examples = "Peter gave his book to his sister Mary yesterday in Berlin. She is a young girl. He wants to make her happy"
-    # examples = "Palmer College of Chiropractic in Davenport must readmit Aaron Cannon and allow him to use an assistant to interpret X-rays and other medical images"
-    # examples = "5-2 decision forces school to readmit student Aaron Cannon and allow him to use X-rays and other medical images"
-    # examples = "In the twilight, I am horrified to see a wolf howling at the end of the garden."
-    # examples = "The Seasiders have been struggling to recruit players to fulfil their opening fixture but on Wednesday they completed a deal for Ishmael Miller and John Lundstram has joined on loan from Everton."
-    # examples = "John can't keep up with Mary 's rapid mood swings"
-    # examples = "South Korea has opened its market to foreign cigarettes."
-    # examples = "I can't do it"
-    # examples = "Paul has bought an apple for Anna. She is very happy."
-    # examples = "National Archives Yes, it’s that time again, folks. It’s the first Friday of the month, when for one ever-so-brief moment the interests of Wall Street, Washington and Main Street are all aligned on one thing: Jobs. A fresh update on the U.S. employment situation for January hits the wires at 8:30 a.m. New York time offering one of the most important snapshots on how the economy fared during the previous month. Expectations are for 203,000 new jobs to be created, according to economists polled by Dow Jones Newswires, compared to 227,000 jobs added in February. The unemployment rate is expected to hold steady at 8.3%. Here at MarketBeat HQ, we’ll be offering color commentary before and after the data crosses the wires. Feel free to weigh-in yourself, via the comments section. And while you’re here, why don’t you sign up to follow us on Twitter. Enjoy the show. ||||| Employers pulled back sharply on hiring last month, a reminder that the U.S. economy may not be growing fast enough to sustain robust job growth. The unemployment rate dipped, but mostly because more Americans stopped looking for work. The Labor Department says the economy added 120,000 jobs in March, down from more than 200,000 in each of the previous three months. The unemployment rate fell to 8.2 percent, the lowest since January 2009. The rate dropped because fewer people searched for jobs. The official unemployment tally only includes those seeking work. The economy has added 858,000 jobs since December _ the best four months of hiring in two years. But Federal Reserve Chairman Ben Bernanke has cautioned that the current hiring pace is unlikely to continue without more consumer spending."
-    # examples = "The unemployment rate dropped to 8.2% last month, but the economy only added 120,000 jobs, when 203,000 new jobs had been predicted, according to today's jobs report. Reaction on the Wall Street Journal's MarketBeat Blog was swift: 'Woah!!! Bad number.' The unemployment rate, however, is better news; it had been expected to hold steady at 8.3%. But the AP notes that the dip is mostly due to more Americans giving up on seeking employment."
-
-    # truth_tuples: List[tuple] = model.extract_tuples(examples)
-    # print(truth_tuples)
+    source_text = "National Archives Yes, it’s that time again, folks. It’s the first Friday of the month, when for one ever-so-brief moment the interests of Wall Street, Washington and Main Street are all aligned on one thing: Jobs. A fresh update on the U.S. employment situation for January hits the wires at 8:30 a.m. New York time offering one of the most important snapshots on how the economy fared during the previous month. Expectations are for 203,000 new jobs to be created, according to economists polled by Dow Jones Newswires, compared to 227,000 jobs added in February. The unemployment rate is expected to hold steady at 8.3%. Here at MarketBeat HQ, we’ll be offering color commentary before and after the data crosses the wires. Feel free to weigh-in yourself, via the comments section. And while you’re here, why don’t you sign up to follow us on Twitter. Enjoy the show. ||||| Employers pulled back sharply on hiring last month, a reminder that the U.S. economy may not be growing fast enough to sustain robust job growth. The unemployment rate dipped, but mostly because more Americans stopped looking for work. The Labor Department says the economy added 120,000 jobs in March, down from more than 200,000 in each of the previous three months. The unemployment rate fell to 8.2 percent, the lowest since January 2009. The rate dropped because fewer people searched for jobs. The official unemployment tally only includes those seeking work. The economy has added 858,000 jobs since December _ the best four months of hiring in two years. But Federal Reserve Chairman Ben Bernanke has cautioned that the current hiring pace is unlikely to continue without more consumer spending."
+    gold_summary = "The unemployment rate dropped to 8.2% last month, but the economy only added 120,000 jobs, when 203,000 new jobs had been predicted, according to today's jobs report. Reaction on the Wall Street Journal's MarketBeat Blog was swift: 'Woah!!! Bad number.' The unemployment rate, however, is better news; it had been expected to hold steady at 8.3%. But the AP notes that the dip is mostly due to more Americans giving up on seeking employment."
+    print(model.calculate_summary_score(source_text, gold_summary))
--- tuple_comparison.py
+++ tuple_comparison.py
@@ -1,81 +1,129 @@
 """
-This is a script that implements different string similarity comparison methods. 
+This is a script that implements the tuple comparison algorithm. 
 """
 
 
-from typing import Optional
+from typing import List, Optional
+import numpy as np
 from rouge_score import rouge_scorer
 from jiwer import wer
 import extract_tuples as et
 
 
-class StringSimilarityMethods:
-    source_str: Optional[str]
-    summary_str: Optional[str]
-    method: str
+def exact_match_string_similarity(
+    source_str: Optional[str], summary_str: Optional[str]
+):
+    if source_str and summary_str and source_str == summary_str:
+        similarity_score = 1.0
+    else:
+        similarity_score = 0
+    return similarity_score
 
-    def __init__(
-        self, source_str: Optional[str], summary_str: Optional[str], method: str
-    ):
-        self.source_str = source_str
-        self.summary_str = summary_str
-        self.method = method
 
-    def exact_match_string_similarity(self):
-        if self.source_str and self.summary_str and self.source_str == self.summary_str:
-            similarity_score = 1.0
-        else:
-            similarity_score = 0
-        return similarity_score
+def spacy_string_similarity(source_str: Optional[str], summary_str: Optional[str]):
+    if source_str and summary_str:
+        nlp = et.load_spacy_model()
+        doc1 = nlp(source_str)
+        doc2 = nlp(summary_str)
+        similarity_score = round(doc1.similarity(doc2), 2)
+    else:
+        similarity_score = 0
+    return similarity_score
 
-    def spacy_string_similarity(self):
-        if self.source_str and self.summary_str:
-            nlp = et.load_spacy_model()
-            doc1 = nlp(self.source_str)
-            doc2 = nlp(self.summary_str)
-            similarity_score = round(doc1.similarity(doc2), 2)
-        else:
-            similarity_score = 0
-        return similarity_score
 
-    def rouge_precision_string_similarity(self):
-        if self.source_str and self.summary_str:
-            scorer = rouge_scorer.RougeScorer(["rouge1"], use_stemmer=True)
-            scores = scorer.score(self.source_str, self.summary_str)
-            precision = scores["rouge1"][0]
-            similarity_score = round(precision, 2)
-        else:
-            similarity_score = 0
-        return similarity_score
+def rouge_precision_string_similarity(
+    source_str: Optional[str], summary_str: Optional[str]
+):
+    if source_str and summary_str:
+        scorer = rouge_scorer.RougeScorer(["rouge1"], use_stemmer=True)
+        scores = scorer.score(source_str, summary_str)
+        precision = scores["rouge1"][0]
+        similarity_score = round(precision, 2)
+    else:
+        similarity_score = 0
+    return similarity_score
 
-    def character_error_rate_string_similarity(self):
-        if self.source_str and self.summary_str:
-            error = wer(self.source_str, self.summary_str)
-            similarity_score = round(error, 2)
-        else:
-            similarity_score = 0
-        return similarity_score
 
-    def calculate_string_similarity(self) -> float:
-        methods = {
-            "exact": self.exact_match_string_similarity,
-            "spacy": self.spacy_string_similarity,
-            "rouge": self.rouge_precision_string_similarity,
-            "CER": self.character_error_rate_string_similarity,
-        }
-        return methods.get(self.method)()
+def character_error_rate_string_similarity(
+    source_str: Optional[str], summary_str: Optional[str]
+):
+    if source_str and summary_str:
+        error = wer(source_str, summary_str)
+        similarity_score = round(error, 2)
+    else:
+        similarity_score = 0
+    return similarity_score
+
+
+def calculate_string_similarity(
+    source_str: Optional[str], summary_str: Optional[str], method: str
+) -> float:
+    methods = {
+        "exact": exact_match_string_similarity,
+        "spacy": spacy_string_similarity,
+        "rouge": rouge_precision_string_similarity,
+        "CER": character_error_rate_string_similarity,
+    }
+    return methods.get(method)(source_str, summary_str)
+
+
+def compare_two_tuples(
+    source_tuple: tuple, generated_tuple: tuple, string_comparison_method: str
+) -> float:
+    """
+    This function calculates consistency score of two tuples.
+    """
+    # hard-coded values for the semantic roles
+    weights = [1 / 3, 0, 1 / 3, 1 / 3, 0, 0, 0]
+    indic = np.array([1 if x else 0 for x in generated_tuple])
+    pairwise_similarity_scores = [
+        calculate_string_similarity(source_str, generated_str, string_comparison_method)
+        for source_str, generated_str in zip(source_tuple, generated_tuple)
+    ]
+
+    normalized_weight = 1 / (np.sum(indic * weights))
+    consistency_score = normalized_weight * np.sum(
+        [indic * pairwise_similarity_scores * weights]
+    )
+    return round(consistency_score, 2)
+
+
+def compare_tuple_with_relevant_tuples(
+    relevant_source_tuples: List[tuple],
+    generated_tup: tuple,
+    string_comparison_method: str,
+) -> float:
+    """
+    This function compares a generated tuple with all of its relevant tuples from source text and takes the max as final score.
+    """
+    tuple_final_score = 0
+    for relevant_source_tup in relevant_source_tuples:
+        consistency_score = compare_two_tuples(
+            source_tuple=relevant_source_tup,
+            generated_tuple=generated_tup,
+            string_comparison_method=string_comparison_method,
+        )
+        if consistency_score >= tuple_final_score:
+            tuple_final_score = consistency_score
+
+    return tuple_final_score
 
 
 if __name__ == "__main__":
-    # compare = StringSimilarityMethods("apple is good", "Apple good", "CER")
-    # compare = StringSimilarityMethods("apple good", "apple is good", "CER")
-    # compare = StringSimilarityMethods("a", None, "CER")
-    # compare = StringSimilarityMethods(None, "a", "CER")
-    # compare = StringSimilarityMethods(None, None, "CER")
-    # compare = StringSimilarityMethods(None, None, "CER")
-    # compare = StringSimilarityMethods("mueller", "robert mueller", "rouge")
-    compare = StringSimilarityMethods("mueller", "robert mueller", "spacy")
-    # compare = StringSimilarityMethods("creature", "alligator", "spacy")
+    print(
+        compare_two_tuples(
+            ("Hans", None, "bought", "a vase", "sister", None, None),
+            ("Hans", None, "send", "a vase", None, None, None),
+            string_comparison_method="rouge",
+        )
+    )
 
-    print(compare.calculate_string_similarity())
+    print(calculate_string_similarity("apple is good", "Apple good", "CER"))
+    print(calculate_string_similarity("apple good", "apple is good", "CER"))
+    print(calculate_string_similarity("a", None, "CER"))
+    print(calculate_string_similarity(None, "a", "CER"))
+    print(calculate_string_similarity(None, None, "CER"))
+
+    print(calculate_string_similarity("a big apple", "apple", "CER"))
+    print(calculate_string_similarity("a big apple", "apple", "rouge"))
 
