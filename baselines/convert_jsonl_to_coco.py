0926d6ca34908054aeb1227ea237e18401b29189
Dennis Aumiller <dennis.aumiller@gmx.de>
2023-05-22 23:52:55 +0200

    Adjusting more folder paths of outputs.

--- baselines/convert_jsonl_to_coco.py
+++ baselines/convert_jsonl_to_coco.py
@@ -2,24 +2,23 @@
 Converter script to transform the QAGS/SummEval data into a format compatible with the CoCo paper.
 """
 
-import os
 import json
 
 
 def convert_qags_cnndm():
-    with open("../qags-cnndm.jsonl", "r") as f:
+    with open("../data/qags-cnndm.jsonl", "r") as f:
         lines = list(f)
 
     # Empty existing files
-    # FIXME: May have to adjut these paths!
-    with open("source-qags-cnndm.txt", "w") as f:
+    with open("./coco_scores/source-qags-cnndm.txt", "w") as f:
         pass
-    with open("summary-qags-cnndm.txt", "w") as f:
+    with open("./coco_scores/summary-qags-cnndm.txt", "w") as f:
         pass
 
     for line in lines:
         sample = json.loads(line)
-        with open("source-qags-cnndm.txt", "a") as source, open("summary-qags-cnndm.txt", "a") as summ:
+        with open("./coco_scores/source-qags-cnndm.txt", "a") as source, \
+                open("./coco_scores/summary-qags-cnndm.txt", "a") as summ:
             source.write(sample["article"] + "\n")
             summary_text = ""
             for sentence in sample["summary_sentences"]:
@@ -28,19 +27,21 @@ def convert_qags_cnndm():
             summary_text = summary_text.strip(" ") + "\n"
             summ.write(summary_text)
 
+
 def convert_qags_xsum():
-    with open("../qags-xsum.jsonl", "r") as f:
+    with open("../data/qags-xsum.jsonl", "r") as f:
         lines = list(f)
 
     # Empty existing files
-    with open("source-qags-xsum.txt", "w") as f:
+    with open("./coco_scores/source-qags-xsum.txt", "w") as f:
         pass
-    with open("summary-qags-xsum.txt", "w") as f:
+    with open("./coco_scores/summary-qags-xsum.txt", "w") as f:
         pass
 
     for line in lines:
         sample = json.loads(line)
-        with open("source-qags-xsum.txt", "a") as source, open("summary-qags-xsum.txt", "a") as summ:
+        with open("./coco_scores/source-qags-xsum.txt", "a") as source, \
+                open("./coco_scores/summary-qags-xsum.txt", "a") as summ:
             source.write(sample["article"] + "\n")
             summary_text = ""
             for sentence in sample["summary_sentences"]:
@@ -51,18 +52,19 @@ def convert_qags_xsum():
 
 
 def convert_summeval():
-    with open("../summeval.jsonl", "r") as f:
+    with open("../data/summeval.jsonl", "r") as f:
         lines = list(f)
 
     # Empty existing files
-    with open("source-summeval.txt", "w") as f:
+    with open("./coco_scores/source-summeval.txt", "w") as f:
         pass
-    with open("summary-summeval.txt", "w") as f:
+    with open("./coco_scores/summary-summeval.txt", "w") as f:
         pass
 
     for line in lines:
         sample = json.loads(line)
-        with open("source-summeval.txt", "a") as source, open("summary-summeval.txt", "a") as summ:
+        with open("./coco_scores/source-summeval.txt", "a") as source, \
+                open("./coco_scores/summary-summeval.txt", "a") as summ:
             source.write(sample["text"].strip(" ") + "\n")
             summ.write(sample["decoded"] + "\n")
 
