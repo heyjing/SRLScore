f2b14d2677fb6a5f7108dae866bb36b698d38679
Dennis Aumiller <dennis.aumiller@gmx.de>
2023-05-22 17:42:43 +0200

    Include description on where to run script.

--- coco_commands.sh
+++ coco_commands.sh
@@ -1,3 +1,6 @@
+# Move this file, such that it is in the same folder as the `run-coco.py` script, which we took from the original work by Xie et al.
+# Repository URL: https://github.com/xieyxclack/factual_coco
+
 python3 run_coco.py --model_path ./bart.large.cnn/ --data_path ./data/qags-cnndm --output_file qags-cnndm-span-scores.txt --mask span
 python3 run_coco.py --model_path ./bart.large.cnn/ --data_path ./data/qags-cnndm --output_file qags-cnndm-sent-scores.txt --mask sent
 
