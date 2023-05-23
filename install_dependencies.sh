a2899beedfddb10f130197a3086d18e6f068cd7a
Dennis Aumiller <dennis.aumiller@gmx.de>
2023-05-23 14:37:13 +0200

    Add spacy models to dependency installation script.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -4,3 +4,5 @@ python3 -m pip install allennlp
 python3 -m pip install allennlp-models
 python3 -m pip install typing-extensions<=4.5.0
 python3 -m pip install rouge-score
+python3 -m spacy download en-core-web-sm
+python3 -m spacy download en-core-web-lg
