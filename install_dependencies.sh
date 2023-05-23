ef4b37c728738c8cb309329cc056d5ec887087e9
Dennis Aumiller <dennis.aumiller@gmx.de>
2023-05-23 14:27:57 +0200

    Clarifying installation script.

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -1,4 +1,5 @@
-
+# Script to install the required dependencies in a way that works; notably, a downgrade of typing-extensions
+# is currently required, see https://github.com/pydantic/pydantic/issues/545#issuecomment-1558685292
 python3 -m pip install allennlp
 python3 -m pip install allennlp-models
 python3 -m pip install typing-extensions<=4.5.0
